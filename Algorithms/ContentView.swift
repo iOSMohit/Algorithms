a7d4c7a360a796c95fb3ae5f801e70f10eb2e3ec
Mohit Kumar <developer.mohitkmr@gmail.com>
2023-01-04 04:11:39 +0530

    Min Heap Implemented

--- Algorithms/ContentView.swift
+++ Algorithms/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     private let heap = MaxHeap<Int>()
+    private let minHeap = MinHeap<Int>()
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -18,8 +19,15 @@ struct ContentView: View {
         }
         .padding()
         .onAppear {
+            print("Max Heap")
             heap.buildHeap(elements: [16, 8, 18, 22, 41, 6, 16, 53, 60, 32, 86, 93, 75])
             heap.printHeap()
+            print("")
+            
+            print("Min Heap")
+            minHeap.buildHeap(elements: [16, 8, 18, 22, 41, 6, 16, 53, 60, 32, 86, 93, 75])
+            minHeap.printHeap()
+            print("")
         }
     }
 }
--- Algorithms/Heap/Heap.swift
+++ Algorithms/Heap/Heap.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+protocol Heap {
+    associatedtype Element
+    
+    var elements: [Element] { get set }
+    var size: Int { get }
+    
+    func buildHeap(elements: [Element])
+    func printHeap()
+    
+    func swap(fromIndex: Int, toIndex: Int)
+}
+
 final class MaxHeap<Element: Comparable> {
     
     // MARK: - Properties
@@ -98,4 +110,90 @@ final class MaxHeap<Element: Comparable> {
 
 final class MinHeap<Element: Comparable> {
     
+    /// MARK: - Properties
+    private var elements = [Element]()
+    private var size: Int {
+        return elements.count
+    }
+    
+    /// MARK: - Public methods
+    func buildHeap(elements: [Element]) {
+        self.elements = elements
+        for index in stride(from: size/2, through: 0, by: -1) {
+            minHeapify(index: index)
+        }
+    }
+    private func delete(at index: Int) {
+        guard !elements.isEmpty, elements.indices.contains(index) else { return }
+        
+        self.elements[index] = self.elements[size-1]
+        self.elements.removeLast()
+        
+        self.minHeapify(index: index)
+    }
+    func delete(value: Element) {
+        if let index = self.elements.firstIndex(of: value) {
+            delete(at: index)
+        }
+    }
+    func insert(value: Element) {
+        self.elements.append(value)
+        
+        heapifyUp(index: size-1)
+    }
+    func printHeap() {
+        for item in elements {
+            print(item, terminator: " ")
+        }
+    }
+    
+    /// MARK: - Private helper methods
+    private func heapifyUp(index: Int) {
+        if index == 0 {
+            // We are alreadyx at top of the heap
+            return
+        }
+        
+        let parent = parent(of: index)
+        if elements[index] < elements[parent] {
+            swap(fromIndex: parent, toIndex: index)
+            heapifyUp(index: parent)
+        }
+    }
+    private func minHeapify(index: Int) {
+        let left = left(of: index)
+        let right = right(of: index)
+        var smallest = index
+        
+        if left < size && elements[left] <= elements[index] {
+            smallest = left
+        }
+        
+        if right < size && elements[right] < elements[smallest] {
+            smallest = right
+        }
+        
+        if smallest != index {
+            swap(fromIndex: smallest, toIndex: index)
+            minHeapify(index: smallest)
+        }
+    }
+
+
+    private func left(of index: Int) -> Int {
+        return (2*index)+1
+    }
+    private func right(of index: Int) -> Int {
+        return (2*index)+2
+    }
+    private func parent(of index: Int) -> Int {
+        return (index-1)/2
+    }
+
+
+    private func swap(fromIndex: Int, toIndex: Int) {
+        let tempValue = elements[fromIndex]
+        elements[fromIndex] = elements[toIndex]
+        elements[toIndex] = tempValue
+    }
 }
