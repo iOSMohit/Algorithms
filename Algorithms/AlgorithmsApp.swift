9f7906ddb37a4fa691db3c63f1f84669795c747b
Mohit Kumar <developer.mohitkmr@gmail.com>
2023-01-03 18:22:37 +0530

    - First Commit

---
+++ Algorithms/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Mohit Kumar on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
